df7c72f189fcb03a57d750e7bddff2826ac5efe4
neripark <neripark@gmail.com>
2019-11-18 16:48:00 +0900

    description advanced

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const template = (str, ...value) => {
-  // 第１引数 str は文字列が入った配列。だから必ず一つで良い。
-  // 第２引数はオブジェクト。変数は名前と中身の値がセットのため。
+const template = (str, ...values) => {
+  // 第１引数 str は、テンプレートリテラルで渡された中身のうち、固定文字列部分のみが入ってくる。
+  // ただの文字列なので、配列にしてひとまとめに受け取る作りでよい。だからstrという引数一つで良い。
+  // 第２（以降）引数 values は、テンプレートリテラルで渡された中身のうち、変数部分(${var_name})が別々に入ってくる。
+  // 変数は名前と中身の値がセットのため、オブジェクトで受け取らないといけない。
 
   console.log('--- str ---');
   str.forEach((e) => {
     console.log(e);
   });
 
-  console.log('--- value ---');
-  value.forEach((e) => {
+  console.log('--- values ---');
+  values.forEach((e) => {
     console.log(e);
   });
 }
@@ -20,13 +22,13 @@ const fuga = "uuuuu";
 template`first: ${tmp} second: ${hoge},${fuga}.`;
 
 
-// どうしてスプレッド演算子をつけないといけないのか？の答えがこれ。
+// どうして第２引数のvaluesにはスプレッド演算子をつけないといけないのか？の答えがこれ。
 // つけないと、単純に引数の個数が可変にならず、１つ目しか受け取れないというだけ。
 
-// const spread = (...value) => {
-//   console.log(value);
-//   console.log('value.length', value.length);
-//   value.forEach((e) => {
+// const spread = (...values) => {
+//   console.log(values);
+//   console.log('values.length', values.length);
+//   values.forEach((e) => {
 //     console.log(e);
 //   })
 // }
